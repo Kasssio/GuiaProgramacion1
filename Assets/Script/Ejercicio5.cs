02066f3ac9af21a19da05fbf01b6bbdf2174cb49
Lucas Estrada <luquiest@gmail.com>
2022-03-29 14:32:39 -0300

    ejercicios 1 a 7

--- Assets/Script/Ejercicio5.cs
+++ Assets/Script/Ejercicio5.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Ejercicio5 : MonoBehaviour
 {
+    public int num1;
+    public int num2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(num1 + " / " + num2 + " = " + (num1 / num2) + "");
     }
 
     // Update is called once per frame
---
+++ Assets/Script/Ejercicio7.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio7 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    public int num3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 > num2 && num1 > num3)
+        {
+            Debug.Log("El numero mas grande es " + num1 + "");
+        }
+
+        else if (num2 > num1 && num2 > num3)
+        {
+            Debug.Log("El numero mas grande es " + num2 + "");
+        }
+
+        else if (num1 == num2 && num1 == num3)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+
+        else
+        {
+            Debug.Log("El numero mas grande es " + num3 + "");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
